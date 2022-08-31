76e6a16a062cee15e81d60b207118f87ef63755e
Haiseong Jeong <wjdgotjd9908@gmail.com>
2022-08-31 14:37:20 +0900

    asset_16_to_32

--- so_long/main.c
+++ so_long/main.c
@@ -6,7 +6,7 @@
 /*   By: hajeong <hajeong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 21:29:09 by hajeong           #+#    #+#             */
-/*   Updated: 2022/08/28 20:39:47 by hajeong          ###   ########.fr       */
+/*   Updated: 2022/08/31 14:35:17 by hajeong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -136,13 +136,13 @@ int			main(void)
 	img8 = mlx_xpm_file_to_image(mlx, "./asset/mario3.xpm", &img_width, &img_height);
 	img9 = mlx_xpm_file_to_image(mlx, "./asset/mario4.xpm", &img_width, &img_height);
 	mlx_put_image_to_window(mlx, win, img, 0, 0);
-	mlx_put_image_to_window(mlx, win, img3, 16, 0);
-	mlx_put_image_to_window(mlx, win, img4, 32, 0);
-	mlx_put_image_to_window(mlx, win, img5, 0, 16);
-	mlx_put_image_to_window(mlx, win, img6, 16, 16);
-	mlx_put_image_to_window(mlx, win, img7, 32, 16);
-	mlx_put_image_to_window(mlx, win, img7, 128, 128);
-	mlx_put_image_to_window(mlx, win, img7, 128, 144);
+	mlx_put_image_to_window(mlx, win, img3, 32, 0);
+	mlx_put_image_to_window(mlx, win, img4, 64, 0);
+	mlx_put_image_to_window(mlx, win, img5, 0, 32);
+	mlx_put_image_to_window(mlx, win, img6, 128, 32);
+	mlx_put_image_to_window(mlx, win, img7, 32, 32);
+	mlx_put_image_to_window(mlx, win, img7, 64, 64);
+	mlx_put_image_to_window(mlx, win, img7, 128, 64);
 	mlx_loop(mlx);
 
 
